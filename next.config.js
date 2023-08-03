c451b4571f57e4bc1a7ef96d57d6d47e229a5931
Uichae <ghddmlco@gmail.com>
2023-08-03 15:24:17 +0900

    Badge, input, Searchfield, Textarea 생성

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+// const withNextra = require('nextra')({
+//   theme: 'nextra-theme-docs',
+//   themeConfig: './theme.config.tsx',
+// })
 
-module.exports = withNextra()
+// module.exports = withNextra()
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  staticImage: true,
+});
+
+module.exports = withNextra();
