d0eca40d84f52ef4f449d60a16b52ba39e1a7487
Uichae <ghddmlco@gmail.com>
2023-08-04 14:30:01 +0900

    Update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-// const withNextra = require('nextra')({
-//   theme: 'nextra-theme-docs',
-//   themeConfig: './theme.config.tsx',
-// })
-
-// module.exports = withNextra()
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  staticImage: true,
 });
 
 module.exports = withNextra();
