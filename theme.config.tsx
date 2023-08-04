e6e1f7ecfc8a60ec1decbbafff488361c4b9a344
Eunyoung Park <108717962+Zoey425@users.noreply.github.com>
2023-08-04 13:15:33 +0900

    Button, BreadCrumb, Icon.mdx 생성 (#2)
    * Button, BreadCrumb, Icon.mdx 생성
    
    * Button, BreadCrumb, Icon.mdx ui 수정
    
    * Header 수정
    
    * Logo 컴포넌트 .mdx 생성
    
    * Nav.mdx 생성

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <h1><img src="/logo_nextra.svg" width="220" /></h1>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
+    link: 'https://github.com/sniperfactory-official/sfac-designkit-react',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
